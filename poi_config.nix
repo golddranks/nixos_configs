d503cd4ab439de3b7634919b96155cda5322b8d8
Pyry Kontio <pyry.kontio@drasa.eu>
2021-03-15 00:50:06 +0900

    Don't advertise samba on poi

--- poi_config.nix
+++ poi_config.nix
@@ -138,17 +138,6 @@
     extraServiceFiles = {
       ssh = "${pkgs.avahi}/etc/avahi/services/ssh.service";
       sftp = "${pkgs.avahi}/etc/avahi/services/sftp-ssh.service";
-      smb = ''
-        <?xml version="1.0" standalone='no'?><!--*-nxml-*-->
-        <!DOCTYPE service-group SYSTEM "avahi-service.dtd">
-        <service-group>
-          <name replace-wildcards="yes">%h</name>
-          <service>
-            <type>_smb._tcp</type>
-            <port>445</port>
-          </service>
-        </service-group>
-     '';
     };
   };
 
