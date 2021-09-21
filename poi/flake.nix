7010792cbcac7e84781876cf12103921bcafb0df
Pyry Kontio <pyry.kontio@drasa.eu>
2021-09-22 00:30:12 +0900

    Still doesn't work.

--- poi/flake.nix
+++ poi/flake.nix
@@ -1,15 +1,15 @@
 {
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-21.05";
   inputs.nixos-hardware.url = "github:NixOS/nixos-hardware/master";
-  inputs.checkup.url = "github:golddranks/checkup-nix";
+  #inputs.checkup.url = "github:golddranks/checkup-nix";
 
-  outputs = { self, nixpkgs }: {
-    nixosConfigurations.container = nixpkgs.lib.nixosSystem {
+  outputs = { self, nixpkgs, nixos-hardware }: {
+    nixosConfigurations.poi = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
       modules = [
         ./configuration.nix
         nixos-hardware.nixosModules.raspberry-pi-4
-        checkup.nixosModules.aarch64-linux
+        #checkup.nixosModules.aarch64-linux
       ];
     };
   };
