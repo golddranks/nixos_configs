259cd374ff9639fa5407faf8b928c1c02ddbd3f5
Pyry Kontio <pyry.kontio@drasa.eu>
2021-09-21 23:17:57 +0900

    Flakeize poi config

--- poi/poi_config.nix
+++ poi/poi_config.nix
@@ -3,7 +3,7 @@
 # and in the NixOS manual (accessible by running ‘nixos-help’).
 
 { config, pkgs, ... }:
-
+let chkp = builtins.getFlake (toString /home/kon/repos/checkup-nix); in
 {
 
   nix = {
@@ -17,6 +17,7 @@
     [ # Include the results of the hardware scan.
       /etc/nixos/hardware-configuration.nix
       "${fetchTarball "https://github.com/NixOS/nixos-hardware/archive/936e4649098d6a5e0762058cb7687be1b2d90550.tar.gz" }/raspberry-pi/4"
+      chkp.nixosModules.aarch64-linux
     ];
 
   networking.hostName = "poi"; # Define your hostname.
