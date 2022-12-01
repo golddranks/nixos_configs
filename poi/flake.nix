46efdad16a9b7f78a5d0a83dfdee578f300f68c1
Pyry Kontio <pyry.kontio@drasa.eu>
2022-12-02 01:16:35 +0900

    poi→nixos 22.11

--- poi/flake.nix
+++ poi/flake.nix
@@ -1,5 +1,5 @@
 {
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-22.05";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-22.11";
   inputs.nixos-hardware.url = "github:NixOS/nixos-hardware/master";
 
   outputs = { self, nixpkgs, nixos-hardware }: {
