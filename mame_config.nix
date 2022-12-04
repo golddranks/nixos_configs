796415411d8865135833ce0a6f616095f89c3140
Pyry Kontio <pyry.kontio@drasa.eu>
2022-12-04 23:58:31 +0900

    juuh

--- mame_config.nix
+++ mame_config.nix
@@ -295,10 +295,10 @@ let dfree = pkgs.writeShellScriptBin "dfree" ''
           extraConfig = "autoindex on;";
         };
         "/protected/" = {
-          tryFiles = "$uri /protected/";
+          tryFiles = "$uri /protected/warning";
           basicAuthFile = "/var/lib/nginx/secrets/webshare.drasa.eu_protected_password";
         };
-        "=/protected" =
+        "=/protected/warning" =
         let
           index = builtins.toFile "index.html" "This is a protected folder. A password is required, and the file index is not shown.";
         in {
