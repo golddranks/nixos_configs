c5116674091dc0c4a3f4ce60188c24bc154a9bb3
Pyry Kontio <pyry.kontio@drasa.eu>
2023-08-01 02:27:50 +0900

    commit

--- poi/rebuild.sh
+++ poi/rebuild.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-nix flake update path:/home/kon/nixos_configs/poi
-sudo nixos-rebuild switch --flake path:/home/kon/nixos_configs/poi
+#nix flake update path:/home/kon/nixos_configs/poi
+sudo nixos-rebuild switch --flake path:/home/kon/nixos_configs/poi --update-input nixpkgs --commit-lock-file
