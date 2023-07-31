db5209b413ba1607232f9d229e1e6b0cc88d0217
Pyry Kontio <pyry.kontio@drasa.eu>
2023-08-01 02:23:19 +0900

    Added msg

--- should_reboot.sh
+++ should_reboot.sh
@@ -6,8 +6,10 @@ built="$(readlink /nix/var/nix/profiles/system/{initrd,kernel,kernel-modules})"
 printf "System booted with:\n$booted\n"
 echo
 printf "Current build is:\n$built\n"
+echo
 
 if [ "$booted" != "$built" ]; then
-	echo
 	echo "They are different, so you should perhaps reboot."
+else
+	echo "No differences!"
 fi
