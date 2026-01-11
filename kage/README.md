c9d0694df1ea3e1fef91b349153fd49f455f0288
Pyry Kontio <pyry.kontio@drasa.eu>
2026-01-12 03:16:28 +0900

    Add Kage

---
+++ kage/README.md
@@ -0,0 +1,7 @@
+How to apply the configuration?
+
+Run
+
+```
+sudo darwin-rebuild switch --flake .
+```
---
+++ kage/configuration.nix
@@ -0,0 +1,51 @@
+{ pkgs, ... }:
+{
+  environment.systemPackages = with pkgs; [
+    git
+    vim
+    nixd
+    nil
+    nixfmt-rfc-style
+    ripgrep
+    ripgrep-all
+    nkf
+    sd
+    fd
+    jq
+    coreutils
+    shellcheck
+    cargo-tarpaulin
+    cargo-fuzz
+    ollama
+    tokei
+    audacity
+    ffmpeg
+    deno
+    nodejs
+    texliveFull
+    uv
+    ghostscript
+  ];
+
+  networking.hostName = "kage";
+  nixpkgs.hostPlatform = "aarch64-darwin";
+  nix.enable = false;
+  security.pam.services.sudo_local.touchIdAuth = true;
+
+  system = {
+    primaryUser = "kon";
+    stateVersion = 5;
+    defaults = {
+      finder = {
+        FXPreferredViewStyle = "Nlsv";
+        AppleShowAllExtensions = true;
+      };
+      dock = {
+        autohide = true;
+        mru-spaces = false;
+      };
+      screencapture.location = "~/Pictures/screenshots";
+      screensaver.askForPasswordDelay = 10;
+    };
+  };
+}
